6be783b1a78bef3a51ac08d62556a492f9a322b2
Sanjeev <bajrasanjeev2013@gmai.com>
2026-04-28 10:42:52 +1000

    GREEN: Implement StringUtils class methods

--- src/string_utils.py
+++ src/string_utils.py
@@ -1,28 +1,17 @@
-# class StringUtils:
-#     def reverse(self, text):
-#         return text[::-1]
-
-#     def is_palindrome(self, text):
-#         if not text:
-#             return True
-#         cleaned = text.replace(" ", "").lower()
-#         return cleaned == cleaned[::-1]
-
-#     def count_words(self, text):
-#         if not text.strip():
-#             return 0
-#         return len(text.split())
-
-#     def count_character(self, text, char):
-#         return text.count(char)
-
 class StringUtils:
-    def reverse(self, s):
-        return s[::-1]
+    def reverse(self, text):
+        return text[::-1]
 
-    def is_palindrome(self, s):
-        s = s.lower().replace(" ", "")
-        return s == s[::-1]
+    def is_palindrome(self, text):
+        if not text:
+            return True
+        cleaned = text.replace(" ", "").lower()
+        return cleaned == cleaned[::-1]
 
-    def count_words(self, s):
-        return len(s.split())
+    def count_words(self, text):
+        if not text.strip():
+            return 0
+        return len(text.split())
+
+    def count_character(self, text, char):
+        return text.count(char)
